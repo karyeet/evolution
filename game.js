dfd4beb0d8f82cd806b6fee89a64b9a6e5e80f0c
Kareem <28524112+karyeet@users.noreply.github.com>
2024-04-27 14:03:27 -0700

    n^2 collision detection

--- game.js
+++ game.js
@@ -1,9 +1,5 @@
 "use strict"
 
-const npcHTML = '<div id="idPlaceholder" class="npc">';
-
-const foodHTML = '<div id="idPlaceholder" class="food"></div>';
-
 class food {
     constructor(elementId, positionX, positionY) {
         this.positionX = positionX
@@ -62,12 +58,14 @@ class npc {
         this.speed = 45
         this.angle = 0
         this.rotationSpeed = 5
+        this.foodEaten = 0
     }
 
     moveForward() {
         this.positionX += Math.cos(this.angle * Math.PI / 180) * this.speed;
         this.positionY += Math.sin(this.angle * Math.PI / 180) * this.speed;
         this.checkBounds();
+        this.checkCollision();
         this.element.style.left = this.positionX + 'px';
         this.element.style.top = this.positionY + 'px';
     }
@@ -75,6 +73,7 @@ class npc {
         this.positionX -= Math.cos(this.angle * Math.PI / 180) * this.speed;
         this.positionY -= Math.sin(this.angle * Math.PI / 180) * this.speed;
         this.checkBounds();
+        this.checkCollision();
         this.element.style.left = this.positionX + 'px';
         this.element.style.top = this.positionY + 'px';
     }
@@ -117,7 +116,38 @@ class npc {
         this.angle += this.rotationSpeed
         this.element.style.transform = 'rotate(' + this.angle + 'deg)'
     }
+    checkCollision() {
+        for (const key in game.friendlies) {
+            if (key !== this.element.id) {
+                const otherNpc = game.friendlies[key];
+                const thisRect = this.element.getBoundingClientRect();
+                const otherRect = otherNpc.element.getBoundingClientRect();
+                if (thisRect.left < otherRect.right &&
+                    thisRect.right > otherRect.left &&
+                    thisRect.top < otherRect.bottom &&
+                    thisRect.bottom > otherRect.top) {
+                    this.positionX -= Math.cos(this.angle * Math.PI / 180) * this.speed;
+                    this.positionY -= Math.sin(this.angle * Math.PI / 180) * this.speed;
+                    this.element.style.left = this.positionX + 'px';
+                    this.element.style.top = this.positionY + 'px';
+                }
+            }
+        }
 
+        for (const key in game.consumable) {
+            const food = game.consumable[key];
+            const thisRect = this.element.getBoundingClientRect();
+            const foodRect = food.element.getBoundingClientRect();
+            if (thisRect.left < foodRect.right &&
+                thisRect.right > foodRect.left &&
+                thisRect.top < foodRect.bottom &&
+                thisRect.bottom > foodRect.top) {
+                food.element.remove();
+                delete game.consumable[key];
+                this.foodEaten++;
+            }
+        }
+    }
 }
 
 
@@ -125,6 +155,6 @@ class npc {
 let game = new Game(800, 600);
 game.spawnFriendly('npc1', 50, 100, 100);
 game.spawnFriendly('npc2', 50, 200, 200);
-game.spawnFood('food1', 300, 300);
+game.spawnFood('food1', 0, 300);
 
 game.gameLoop();
