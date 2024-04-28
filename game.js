5ea4cd60deb5efb82a90585bee3f8d5e4c5cdf46
Adil Mohiuddin <amohi9046@gmail.com>
2024-04-28 02:27:11 -0700

    refactor a bit

--- game.js
+++ game.js
@@ -1,7 +1,3 @@
-const border_width = 2;
-
-const TOTALkirbies = 50;
-
 let NEATconfig = {
   model: [
     // Angle to nearest food relative to world, current angle
@@ -14,176 +10,7 @@ let NEATconfig = {
   populationSize: TOTALkirbies,
 };
 
-class npc {
-  constructor(scene, x, y, id) {
-    this.scene = scene;
-    this.sprite = scene.physics.add.sprite(x, y, "dot");
-    this.sprite.npc = this;
-    this.sprite.setScale(0.5);
-    this.cursors = scene.input.keyboard.createCursorKeys();
-    this.hunger = 50;
-    scene.input.keyboard.on("keydown", this.handleKeyDown, this);
-    this.Pangle = 0;
-    this.pX = this.sprite.x;
-    this.pY = this.sprite.y;
-    this.pNearFood = 0;
-    this.nearFood = 0;
-    this.nearAngle = 0;
-
-    this.id = id;
-
-    this.bar_border = this.scene.add.graphics();
-    this.bar_border.fillStyle(0xffffff, 1);
-    this.bar_border.fillRect(
-      -15 - border_width,
-      -this.sprite.height - border_width,
-      this.sprite.width - 5 + border_width * 2,
-      5 + border_width * 2,
-    );
-
-    const hunger_percent = this.hunger / 100;
-    this.bar = this.scene.add.graphics();
-    this.bar.fillStyle(interpolate("#ff0000", "#00ff00", hunger_percent), 1);
-    this.bar.fillRect(-15, -this.sprite.height, this.sprite.width - 5, 5);
-  }
-  movefoward() {
-    var angle = Phaser.Math.DegToRad(this.sprite.angle - 90);
-    this.sprite.setVelocityX(Math.cos(angle) * 200);
-    this.sprite.setVelocityY(Math.sin(angle) * 200);
-  }
-
-  movebackward() {
-    var angle = Phaser.Math.DegToRad(this.sprite.angle - 90);
-    this.sprite.setVelocityX(Math.cos(angle) * -200);
-    this.sprite.setVelocityY(Math.sin(angle) * -200);
-  }
-
-  turnleft() {
-    this.Pangle = this.sprite.angle;
-    this.sprite.angle -= 4;
-  }
-  turnright() {
-    this.Pangle = this.sprite.angle;
-    this.sprite.angle += 4;
-  }
-
-  NearestFood() {
-    var nearestDist = Number.MAX_VALUE;
-    var nearestAngle = 0;
-    for (let foodItem of food.getChildren()) {
-      var dist = Phaser.Math.Distance.Between(
-        this.sprite.x,
-        this.sprite.y,
-        foodItem.x,
-        foodItem.y,
-      );
-      if (dist < nearestDist) {
-        nearestDist = dist;
-        nearestAngle = Phaser.Math.Angle.Between(
-          this.sprite.x,
-          this.sprite.y,
-          foodItem.x,
-          foodItem.y,
-        );
-      }
-    }
-    return {
-      distance: nearestDist,
-      angle: nearestAngle,
-    };
-  }
-
-  //generateRandomArray() {
-  //    var randomArray = [];
-  //    for (var i = 0; i < 24; i++) {
-  //        randomArray.push(Math.random());
-  //    }
-  //    this.Brain = randomArray;
-  //    return randomArray;
-  //}
-
-  move(decision) {
-    if (decision == 0) {
-      this.turnleft();
-      this.movefoward();
-    } else if (decision == 1) {
-      this.turnright();
-      this.movefoward();
-    }
-  }
-
-  update() {
-    this.makeBar();
-    this.pX = this.sprite.x;
-    this.pY = this.sprite.y;
-    this.pNearFood = this.nearFood;
-    var temp = this.NearestFood();
-    this.nearFood = temp.distance;
-    this.nearAngle = temp.angle;
-
-    let generation = Math.floor(this.id / TOTALkirbies);
-
-    //console.log("inputs", [this.nearAngle*(180/Math.PI), this.sprite.angle-90], this.id%TOTALkirbies)
-    //console.log("this.id", this.id)
-    //console.log("generation", generation)
-    //console.log("neatstore", NEATstore)
-    NEATstore[generation].setInputs(
-      [this.nearAngle * 180, this.sprite.angle - 90],
-      this.id % TOTALkirbies,
-    );
-
-    // get decision var action = this.NeuralNetworkMove()
-
-    this.checkBounds();
-  }
-
-  checkBounds() {
-    const config = this.scene.game.config;
-    const npc = this.sprite;
-
-    if (npc.x < 0) {
-      npc.x = 0;
-    } else if (npc.x > config.width) {
-      npc.x = config.width;
-    }
-
-    if (npc.y < 0) {
-      npc.y = 0;
-    } else if (npc.y > config.height) {
-      npc.y = config.height;
-    }
-  }
-
-  handleKeyDown(event) {
-    if (
-      event.code === "KeyW" ||
-      event.code === "KeyA" ||
-      event.code === "KeyS" ||
-      event.code === "KeyD"
-    ) {
-      event.preventDefault();
-    }
-  }
-
-  makeBar() {
-    this.bar_border.x = this.sprite.x;
-    this.bar_border.y = this.sprite.y;
-
-    const hunger_percent = this.hunger / 150;
-    this.bar.clear();
-    this.bar.fillStyle(interpolate("#ff0000", "#00ff00", hunger_percent), 1);
-    this.bar.fillRect(
-      -15,
-      -this.sprite.height,
-      (this.sprite.width - 5) * hunger_percent,
-      5,
-    );
-    this.bar.x = this.sprite.x;
-    this.bar.y = this.sprite.y;
-  }
-}
-
-var config = {
+const config = {
   type: Phaser.AUTO,
   width: 3000,
   height: 3000,
@@ -201,10 +28,9 @@ var config = {
   },
 };
 
-var game = new Phaser.Game(config);
+const game = new Phaser.Game(config);
 
-var npcs = [];
-var food;
+let npcs = [];
 
 function preload() {
   this.load.image("dot", "./white_blood_cell_1.png");
@@ -218,10 +44,6 @@ let whichhitTrigger = []; // which hit trigger so they dont hit twice
 let NEATstore = []; // store the NEAT objects
 
 function create() {
-  // console.log(this);
-  //player = new npc(this, 200, 200);
-  // background
-
   let bg = this.add.image(0, 0, "background").setOrigin(0, 0);
   bg.displayWidth = this.sys.game.config.width;
   bg.displayHeight = this.sys.game.config.height;
@@ -234,7 +56,9 @@ function create() {
     loop: true,
   });
   npcs = [];
+
   runtimePrint("spawning " + TOTALkirbies);
+
   for (let i = 0; i < TOTALkirbies; i++) {
     npcs.push(
       new npc(
@@ -255,20 +79,6 @@ function create() {
   this.time.addEvent({
     delay: 1000,
     callback: createFood,
-    callback: createFood,
-    callback: createFood,
-    callback: createFood,
-    callback: createFood,
-    callback: createFood,
-    callback: createFood,
-    callback: createFood,
-    callback: createFood,
-    callback: createFood,
-    callback: createFood,
-    callback: createFood,
-    callback: createFood,
-    callback: createFood,
-    callback: createFood,
     callbackScope: this,
     loop: true,
   });
@@ -284,7 +94,7 @@ function update() {
   if (npcs.length < 1) {
     runtimePrint("All NPC's dead");
   }
-  for (let npc of npcs) {
+  for (const npc of npcs) {
     if (!npc) continue;
     npc.update();
     this.physics.overlap(npc.sprite, food, eatFood, null, this);
@@ -378,11 +188,10 @@ function tickHunger() {
 }
 
 function createFood() {
-  var x = Phaser.Math.Between(0, config.width);
-  var y = Phaser.Math.Between(0, config.height);
+  const x = Phaser.Math.Between(0, config.width);
+  const y = Phaser.Math.Between(0, config.height);
 
-  var newFood = food.create(x, y, "food");
-  newFood.setScale(1);
+  food.create(x, y, "food").setScale(1);
 }
 
 function eatFood(sprite, food) {
@@ -390,24 +199,5 @@ function eatFood(sprite, food) {
   food.destroy();
 }
 
-function interpolate(color1, color2, percent) {
-  // Convert the hex colors to RGB values
-  const r1 = parseInt(color1.substring(1, 3), 16);
-  const g1 = parseInt(color1.substring(3, 5), 16);
-  const b1 = parseInt(color1.substring(5, 7), 16);
-
-  const r2 = parseInt(color2.substring(1, 3), 16);
-  const g2 = parseInt(color2.substring(3, 5), 16);
-  const b2 = parseInt(color2.substring(5, 7), 16);
-
-  // Interpolate the RGB values
-  const r = Math.round(r1 + (r2 - r1) * percent);
-  const g = Math.round(g1 + (g2 - g1) * percent);
-  const b = Math.round(b1 + (b2 - b1) * percent);
-
-  // Convert the interpolated RGB values back to a hex color
-  return (1 << 24) + (r << 16) + (g << 8) + b;
-}
-
 runtimePrint = console.log;
-//console.log = () => { }; //disable console.log
+// console.log = () => { }; // disable console.log
---
+++ npc.js
@@ -0,0 +1,119 @@
+const TOTALkirbies = 50;
+const border_width = 2;
+
+class npc {
+    constructor(scene, x, y, id) {
+      this.scene = scene;
+      this.sprite = scene.physics.add.sprite(x, y, "dot");
+      this.sprite.npc = this;
+      this.sprite.setScale(0.5);
+      this.cursors = scene.input.keyboard.createCursorKeys();
+      this.hunger = 50;
+      scene.input.keyboard.on("keydown", this.handleKeyDown, this);
+      this.nearAngle = 0;
+
+      this.id = id;
+
+      this.bar = new bar(scene, this.sprite);
+    }
+
+    movefoward() {
+      const angle = Phaser.Math.DegToRad(this.sprite.angle - 90);
+      this.sprite.setVelocityX(Math.cos(angle) * 200);
+      this.sprite.setVelocityY(Math.sin(angle) * 200);
+    }
+
+    movebackward() {
+      const angle = Phaser.Math.DegToRad(this.sprite.angle - 90);
+      this.sprite.setVelocityX(Math.cos(angle) * -200);
+      this.sprite.setVelocityY(Math.sin(angle) * -200);
+    }
+
+    turnleft() {
+      this.Pangle = this.sprite.angle;
+      this.sprite.angle -= 4;
+    }
+    turnright() {
+      this.Pangle = this.sprite.angle;
+      this.sprite.angle += 4;
+    }
+
+    NearestFood() {
+      let nearestDist = Number.MAX_VALUE;
+      let nearestAngle = 0;
+      for (let foodItem of food.getChildren()) {
+        const dist = Phaser.Math.Distance.Between(
+          this.sprite.x,
+          this.sprite.y,
+          foodItem.x,
+          foodItem.y,
+        );
+        if (dist < nearestDist) {
+          nearestDist = dist;
+          nearestAngle = Phaser.Math.Angle.Between(
+            this.sprite.x,
+            this.sprite.y,
+            foodItem.x,
+            foodItem.y,
+          );
+        }
+      }
+      return {
+        distance: nearestDist,
+        angle: nearestAngle,
+      };
+    }
+
+    move(decision) {
+      if (decision == 0) {
+        this.turnleft();
+        this.movefoward();
+      } else if (decision == 1) {
+        this.turnright();
+        this.movefoward();
+      }
+    }
+
+    update() {
+      this.bar.update();
+
+      this.nearAngle = this.NearestFood().angle;
+
+      let generation = Math.floor(this.id / TOTALkirbies);
+
+      NEATstore[generation].setInputs(
+        [this.nearAngle * 180, this.sprite.angle - 90],
+        this.id % TOTALkirbies,
+      );
+
+      this.checkBounds();
+    }
+
+    checkBounds() {
+      const config = this.scene.game.config;
+      const npc = this.sprite;
+
+      if (npc.x < 0) {
+        npc.x = 0;
+      } else if (npc.x > config.width) {
+        npc.x = config.width;
+      }
+
+      if (npc.y < 0) {
+        npc.y = 0;
+      } else if (npc.y > config.height) {
+        npc.y = config.height;
+      }
+    }
+
+    handleKeyDown(event) {
+      if (
+        event.code === "KeyW" ||
+        event.code === "KeyA" ||
+        event.code === "KeyS" ||
+        event.code === "KeyD"
+      ) {
+        event.preventDefault();
+      }
+    }
+  }
